dc9ff8d225526c0a4632451e5de7db656cd41f57
Valdinei Santos <valdgremio@hotmail.com>
2026-04-22 22:25:37 -0300

    utilizacao modelo decisivo treinado com extrema dificuldade e 0.8 das notas em escala de cinza sem centercrop

--- app.py
+++ app.py
@@ -143,7 +143,8 @@ def predict(image, model):
 
     preprocess = transforms.Compose([
         transforms.Resize((224,224)),
-        transforms.Grayscale(num_output_channels=3),
+        # O ColorJitter aqui vai 'estressar' a imagem para neutralizar o vício da câmera
+        transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2),
         transforms.ToTensor(),
         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
     ])
