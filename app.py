7bb947e1f58dfd911b081302c802dfbda769358b
Valdinei Santos <valdgremio@hotmail.com>
2026-04-22 22:10:18 -0300

    utilizacao modelo decisivo treinado com extrema dificuldade e 0.8 das notas em escala de cinza sem centercrop

--- app.py
+++ app.py
@@ -133,9 +133,16 @@ def predict(image, model):
     #imagem_corrigida = corrigir_balanco_branco(image)
     #imagem_corrigida = tratar_imagem_robusta(image)
     # Transformações (devem ser IGUAIS às do treinamento no Colab)
+    
+    # preprocess = transforms.Compose([
+    #     transforms.Resize(256),
+    #     transforms.CenterCrop(224),
+    #     transforms.ToTensor(),
+    #     transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    # ])
+
     preprocess = transforms.Compose([
-        transforms.Resize(256),
-        transforms.CenterCrop(224),
+        transforms.Resize(224,224),
         transforms.ToTensor(),
         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
     ])
