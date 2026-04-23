aa0b2f61af17c3f18e557409682d8e7331a0cfb1
Valdinei Santos <valdgremio@hotmail.com>
2026-04-22 23:14:44 -0300

    utilizacao modelo decisivo treinado com extrema dificuldade e 0.8 das notas em escala de cinza sem centercrop

--- app.py
+++ app.py
@@ -142,6 +142,7 @@ def predict(image, model):
     # ])
 
     preprocess = transforms.Compose([
+        transforms.ToPILImage(),
         transforms.Resize((224,224)),
         # O ColorJitter aqui vai 'estressar' a imagem para neutralizar o vício da câmera
         #transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2),
