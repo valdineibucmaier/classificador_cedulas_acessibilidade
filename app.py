438d7ac6874faf42ec7f4826ea82c1ed62c7b0de
Valdinei Santos <valdgremio@hotmail.com>
2026-04-23 21:54:15 -0300

    mostrando aula como fazer deploy

--- app.py
+++ app.py
@@ -199,7 +199,7 @@ st.markdown("""
     </style>
 """, unsafe_allow_html=True)
 
-st.title("Identificador de Cédulas v.Prime")
+st.title("Identificador de Cédulas versão.Prime")
 
 # --- AVISO INICIAL DE USO ---
 # Usamos o session_state para que o áudio de boas-vindas toque apenas UMA vez ao abrir
